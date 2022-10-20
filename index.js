dbcefc1064573a83d97cd6c3cb3e7ff404d55c19
shuvo1710 <mdshovo102002@gmail.com>
2022-10-20 23:05:44 +0600

    added news

--- index.js
+++ index.js
@@ -6,12 +6,33 @@ const cors = require('cors')
 app.use(cors())
 
 const category =  require('./data/category.json')
+const news = require('./data/news.json')
 
 app.get('/', (req, res)=>{
     res.send('news API Running')
 });
 app.get('/news-categories',(req,res)=>{
     res.send(category)
+})
+
+app.get('/news/:id',(req,res)=>{
+    const id = req.params.id;
+    const selectedNews= news.find(n=> n._id === id)
+    res.send(selectedNews)   
+})
+app.get('/news',(req,res)=>{
+    res.send(news)
+})
+app.get('/category/:id',(req,res)=>{
+    const id = req.params.id;
+    if(id=== "08"){
+        res.send(news) 
+    }
+    else{
+        const categoryNews= news.filter(n =>n.category_id === id)
+        res.send(categoryNews)
+    }
+    
 })
 
 app.listen(port , ()=>{
